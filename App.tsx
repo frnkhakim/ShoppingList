9f483fa56296f9d30d48b55d3ceaf29ee277ed17
FrankHakim <frank.hakim@digitatanetworks.com>
2026-07-07 20:09:15 +0200

    refactor: restructure App component to utilize HomeScreen for improved organization

--- App.tsx
+++ App.tsx
@@ -1,26 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Text, StyleSheet } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Shopping List</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+                <HomeScreen />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-});
