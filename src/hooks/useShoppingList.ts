c5b2ee9c0c2bd3da624ed62c68450b19a42b0bc6
FrankHakim <frank.hakim@digitatanetworks.com>
2026-07-07 23:32:37 +0200

    feat: refactor useShoppingList hook to support async operations for item management and loading from AsyncStorage

--- src/hooks/useShoppingList.ts
+++ src/hooks/useShoppingList.ts
@@ -1,29 +1,69 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import type { ShoppingItem } from "../types/ShoppingItem";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 export function useShoppingList() {
     const [shoppingItems, setShoppingItems] =
         useState<ShoppingItem[]>([]);
     
-    const addItem = (name: string) => {
-        const newItem: ShoppingItem = {
+   const addItem = async (
+    name: string
+    ) => {
+        const newItem = {
             id: Date.now().toString(),
             name,
         };
 
-        setShoppingItems([
+        const updatedItems = [
             ...shoppingItems,
             newItem,
-        ]);
+        ];
+
+        setShoppingItems(updatedItems);
+
+        await saveItems(updatedItems);
     };
 
-    const deleteItem = (id: string) => {
-        setShoppingItems(
-            shoppingItems.filter(
-                item => item.id !== id
-            )
+    const deleteItem = async (id: string) => {
+        const updatedItems = shoppingItems.filter(
+            item => item.id !== id
         );
+        setShoppingItems(updatedItems);
+        await saveItems(updatedItems);
+    };
+    const loadItems = async () => {
+    try {
+        const value =
+            await AsyncStorage.getItem(
+                "shoppingItems"
+            );
+
+        if (value) {
+            setShoppingItems(
+                JSON.parse(value)
+            );
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+    useEffect(() => {
+    loadItems();
+    }, []);
+    };
+
+    const saveItems = async (
+    items: ShoppingItem[]
+    ) => {
+        try {
+            await AsyncStorage.setItem(
+                "shoppingItems",
+                JSON.stringify(items)
+            );
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return {
