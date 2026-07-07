424cc6e817c90e4a6cfdd7d716c9101dcdfb9805
FrankHakim <frank.hakim@digitatanetworks.com>
2026-07-07 22:08:17 +0200

    feat: enhance ShoppingItem component with styles for better presentation

--- src/components/ShoppingItem.tsx
+++ src/components/ShoppingItem.tsx
@@ -1,13 +1,30 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 interface ShoppingItemProps {
     item: string;
 }
+    
+const styles = StyleSheet.create({
+    item: {
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        width: '100%',
+        backgroundColor: "#f2f2f2",
+        borderRadius: 8,
+        marginBottom: 12,
+    },
+    text: {
+    fontSize: 18,
+    }
+});
 
 export default function ShoppingItem({ item }: ShoppingItemProps) {
     return (
-        <Text>
-            {item}
+        <Text style={styles.item}>
+            <Text style={styles.text}>
+                {item}
+            </Text>
         </Text>
     );
 }
