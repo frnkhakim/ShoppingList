f6e6b92c31dc0e0abfec7e7c5b6243600bc7b235
FrankHakim <frank.hakim@digitatanetworks.com>
2026-07-07 21:52:48 +0200

    feat: pass shoppingItems to ShoppingList component for rendering

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -25,7 +25,7 @@ export default function HomeScreen() {
       <ShoppingInput 
         onAddItem={addItem}
       />
-      <ShoppingList />
+      <ShoppingList items={shoppingItems} />
     </View>
   );
 }
